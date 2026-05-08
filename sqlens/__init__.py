8f93fa4c82ec4be0e23901f5275ae008acb01be8
Junpeng Ju <jujunpeng@users.noreply.github.com>
2026-05-09 05:45:04 +0800

    feat: add get_version() helper function with docstring

--- sqlens/__init__.py
+++ sqlens/__init__.py
@@ -2,3 +2,20 @@
 
 __version__ = "0.1.0"
 __author__ = "sqlens contributors"
+
+
+def get_version() -> str:
+    """Return the current version string of sqlens.
+
+    Returns
+    -------
+    str
+        The version string in ``MAJOR.MINOR.PATCH`` format.
+
+    Examples
+    --------
+    >>> from sqlens import get_version
+    >>> get_version()
+    '0.1.0'
+    """
+    return __version__
